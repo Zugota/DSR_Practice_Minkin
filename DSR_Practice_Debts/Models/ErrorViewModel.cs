87e66c0a95f16a41dd98ad360096bf56c25330a8
Zugota <89272399+Zugota@users.noreply.github.com>
2023-06-26 02:21:19 +0300

    Clean All

--- DSR_Practice_Debts/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace DSR_Practice_Debts.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
